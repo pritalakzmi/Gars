c97bfabe30abfb0f1c2681ed25c262afa812810c
pritalakzmi <2118126@scholar.itn.ac.id>
2023-11-09 20:35:57 +0700

    [Bab 4] Layouts

--- App.js
+++ App.js
@@ -4,3 +4,9 @@ import Profile from './src/screens/Profile';
 export default function App() {
   return <Profile/>;
 }
+
+// import * as React from 'react';
+// import Product from './src/screens/Product';
+// export default function App() {
+//   return <Product/>;
+// }
